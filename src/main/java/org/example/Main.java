2acc5df512348c2b714593a53c9dc7e5086f5da9
oksol-nick <106911859+oksol-nick@users.noreply.github.com>
2024-09-08 19:11:32 +0300

    New maven project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+
+import org.apache.commons.lang3.StringUtils;
+import org.example.MySqareFunction;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(StringUtils.reverse("Hello and welcome!"));
+
+        long a = 6;
+
+        System.out.printf("Квадрат числа %d равен %d\n", a, MySqareFunction.sqareOfNumber(a));
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
